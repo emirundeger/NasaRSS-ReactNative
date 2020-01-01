79e50106e1aa82b8d15c0170973ae212a4838f8e
emirundeger <emirundeger@gmail.com>
2020-01-01 15:48:53 +0000

    delete config

--- app/config/aws-exports.js
+++
@@ -1,8 +0,0 @@
-const awsConfig = {
-  identityPoolId: 'us-east-1:2d8bcbf0-de9d-4f9d-a9f5-bfdab69b8348',
-  region: 'us-east-1',
-  userPoolId: 'us-east-1_jB5ttAlwi',
-  userPoolWebClientId: '4b7fhdltp4oercp7nmjljom4go',
-};
-
-export default awsConfig;
