aeb9d89e947369b529c68d6f47bc700a24702b7d
emirundeger <emirundeger@gmail.com>
2020-01-20 01:28:27 +0000

    Redux provider added

--- index.js
+++ index.js
@@ -4,6 +4,18 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import languageReducer from './app/reducers/languageReducer';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = createStore(languageReducer);
+
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => Root);
